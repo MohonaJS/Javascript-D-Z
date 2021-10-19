f5a63623c30190b83b398180d8a30b7451b7f0da
MohonaJS <mohona.chowdhury@iths.se>
2021-10-19 22:19:52 +0200

    class1homework done

---
+++ class1/main.js
@@ -0,0 +1,63 @@
+/* let num1=80;
+let num2="80";
+let sum=parseInt(80) + parseInt("80"); */
+
+/* let num1 = prompt("number 1: ");
+let num2 = prompt("number 2: ");
+let sum = parseInt(num1) + parseInt(num2)
+
+
+console.log(sum);
+document.write("Your first number is : " + num1 +" and your second number is " + num2 + "the sum is  " + sum ) */
+
+/* let myName= "My name is";
+let name= prompt("enter your name: ");
+ let myAge="My age is ";
+ let age = prompt("enter ur age");
+
+ document.write(myName + " " + name +" " + myAge + " " + age ); */
+
+ 
+ 
+/*  document.write(" Persone 1: "+question);
+ document.write("</br>");
+ document.write(" Persone 2: "+answer); */
+/* 
+ let ask = "When will u fly to riga";
+ let ans=prompt("");
+ let as
+ let reply = prompt("I will fly on : ");
+
+ document.write("Me: " + ask);
+ document.write("My Friend : " +reply ); */
+
+/*  "Now he is ____, I exclaimed. For Heaven's sake, ____ down! Do be quick; and stay ____ the trees till he is fairly in." 
+
+"Now he is here, I exclaimed. For Heaven's sake, hurry down! Do be quick; and stay among the trees till he is fairly in."
+*/
+
+let text1=prompt("give ur first text: ");
+let text2=prompt("give ur second text: ");
+let text3=prompt("give ur third text: ");
+
+document.write("Now he is "+text1+" , I exclaimed. For Heaven's sake, "+text2+"down! Do be quick; and stay  "+text3+"the trees till he is fairly in.")
+
+
+// home Work
+/* "When you choose to place your tag after the line of dialogue, the comma comes at the end of the spoken words, before the closing quotation marks. In this case, following the dialogue with a comma lets the reader know that there's more information to come. After the comma comes the quotation marks to end the dialogue, then a space, then the tag, followed by a closing period to complete the sentence"
+
+"When you (1)____ to place your tag after the line of (2)__, the comma comes at the end of the (3)__ words, before the closing (4) marks. In this case, (5) the dialogue with a comma lets the (6) know that there's more (7) to come. After the (8) comes the (9) marks to end the dialogue, then a space, then the tag, (10)_ by a closing period to complete the sentence." */
+
+let task1 = prompt("enter your first word");
+let task2 = prompt("enter your second word");
+let task3 = prompt("enter your third word");
+let task4 = prompt("enter your fourth word");
+let task5 = prompt("enter your fifth word");
+let task6 = prompt("enter your sixth word");
+let task7 = prompt("enter your seventh word");
+let task8 = prompt("enter your eighth word");
+let task9 = prompt("enter your ninth word");
+let task10 = prompt("enter your tenth word");
+ 
+
+
---
+++ class1HomwWork/main.js
@@ -0,0 +1,17 @@
+// home Work
+/* "When you choose to place your tag after the line of dialogue, the comma comes at the end of the spoken words, before the closing quotation marks. In this case, following the dialogue with a comma lets the reader know that there's more information to come. After the comma comes the quotation marks to end the dialogue, then a space, then the tag, followed by a closing period to complete the sentence"
+
+"When you (1)____ to place your tag after the line of (2)__, the comma comes at the end of the (3)__ the comma comes at the end of the (4) marks. In this case, (5) the dialogue with a comma lets the (6) know that there's more (7) to come. After the (8) comes the (9) marks to end the dialogue, then a space, then the tag, (10)_ by a closing period to complete the sentence." */
+
+let task1 = prompt("enter your first word");
+let task2 = prompt("enter your second word");
+let task3 = prompt("enter your third word");
+let task4 = prompt("enter your fourth word");
+let task5 = prompt("enter your fifth word");
+let task6 = prompt("enter your sixth word");
+let task7 = prompt("enter your seventh word");
+let task8 = prompt("enter your eighth word");
+let task9 = prompt("enter your ninth word");
+let task10 = prompt("enter your tenth word");
+
+document.write("when you" + task1+ "to place your tag after the line of" + task2+ "the comma comes at the end of the" + task3 + "the comma comes at the end of the" + task4 + "marks. In this case, " + task5 + "the dialogue with a comma lets the" + task6 + "know that there's more" + task7 + "to come. After the" + task8 + "comes the" + task9 + "marks to end the dialogue, then a space, then the tag," + task10 + "by a closing period to complete the sentence.")
