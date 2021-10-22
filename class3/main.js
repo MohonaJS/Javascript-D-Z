dd587a04b5a5935d365542924d7ce0a1ce01c168
MohonaJS <mohona.chowdhury@iths.se>
2021-10-22 20:46:30 +0200

    html part

---
+++ class3/main.js
@@ -0,0 +1,51 @@
+/* let fN = prompt("enter ur fi name");
+let lN = prompt("enter ur la name");
+
+let fullName = fN + lN;
+document.write(fullName);
+
+document.write("<br>");
+
+document.write(fullName.length);
+document.write("<br>");
+document.write(fullName.toUpperCase());
+
+
+
+
+
+ */
+
+
+
+
+
+ /* let fname= prompt("enter ur f name");
+ let lname= prompt("enter ur l name");
+
+ let fullName = fname + lname ;
+
+ document.write(fullName);
+
+ document.write("</br>");
+ document.write(fullName.length);
+ document.write("</br>");
+ document.write(fullName.toUpperCase());
+ */
+
+/* 
+ let firstName = prompt("enter ur f name");
+ let lastName = prompt("enter ur l name");
+
+ let fullName = firstName + lastName ; 
+
+ document.write(fullName);
+ document.write("<br>")
+ document.write(fullName.length);
+ document.write("<br>")
+ document.write(fullName.toUpperCase()); */
+
+
+
+ 
+
