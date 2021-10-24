86b0340fb9d2088b35f1c43f92380ee1bf36688c
MohonaJS <mohona.chowdhury@iths.se>
2021-10-24 22:05:11 +0200

    class3 test

---
+++ class3/class3hmw/main.js
@@ -0,0 +1,9 @@
+let firstName = prompt("Enter ur first name: ");
+let lastName = prompt("Enter ur last name: ");
+let fullName = "ur fullname is: " + firstName + " " +lastName;
+
+document.write(fullName);
+document.write("</br>");document.write("</br>");
+document.write(fullName.length);
+document.write("</br>");document.write("</br>");
+document.write(fullName.toUpperCase());
