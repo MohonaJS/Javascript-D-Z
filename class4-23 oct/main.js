d77e6df7afc923e54ed66213c83c2916ceac69d7
MohonaJS <mohona.chowdhury@iths.se>
2021-10-24 14:17:21 +0200

    class4

---
+++ class4-23 oct/main.js
@@ -0,0 +1,7 @@
+const fruits = ["banana", " apple", " peyara", " orange"," halon"];
+
+for(let i = 0; i < fruits.length; i++){
+    document.write("Fruits" + (i+1) + " " + fruits[i] + "</br>");
+
+}
+  
