6fa4fdd6f1953e4ea35e8c8bf4f4144b5617672f
MohonaJS <mohona.chowdhury@iths.se>
2021-10-24 16:47:38 +0200

    array

--- class4-23 oct/main.js
+++
@@ -1,7 +0,0 @@
-const fruits = ["banana", " apple", " peyara", " orange"," halon"];
-
-for(let i = 0; i < fruits.length; i++){
-    document.write("Fruits" + (i+1) + " " + fruits[i] + "</br>");
-
-}
-  
---
+++ class4-23oct/main.js
@@ -0,0 +1,37 @@
+/* const fruits = ["banana", " apple", " peyara", " orange"," halon"];
+
+for(let i = 0; i < fruits.length; i++){
+    document.write("Fruits" + (i+1) + " " + fruits[i] + "</br>");
+
+} */
+  
+const country = ["bangladesh", "sweden", "uk", "usa"];
+const newCountry = ["england", "denmark", "srilanka"];
+
+for(let i = 0; i < newCountry.length; i++){
+    country.push(newCountry[i] + "</br>");
+    document.write(country);
+}
+
+/* let newCountry = country.concat(["madina", "america", "srilanka"]);
+console.log(newCountry); */
+
+
+/* country.shift("denmark")
+country.unshift("khan") */
+// country[] = "india";
+/* country.push("dubai");
+
+country.pop(); */ // tar bort sista value
+
+
+
+/* for(let i = 0; i < country.length; i++){
+    document.write("Country" + (i)+ " : " + country[i] + "</br>");
+
+} */
+
+
+
+
+
