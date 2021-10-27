754f0bfeed1973bd7570ee54a567aa9dee07dbd1
MohonaJS <mohona.chowdhury@iths.se>
2021-10-27 07:17:53 +0200

    forloop

---
+++ class1HomwWork/practiceClassOne/main.js
@@ -0,0 +1,20 @@
+
+/* 
+let name = "sweden";
+
+let result= "";
+
+for(let i = name.length -1; i >= 0; i--){
+    result = result + name[i];
+    console.log(result);
+} */
+let text = ["we live in sweden", " bdcoder"];
+
+let result= [];
+
+for(let i = text.length -1; i >= 0; i--){
+    result = result + text[i];
+    console.log(result);
+}
+
+
