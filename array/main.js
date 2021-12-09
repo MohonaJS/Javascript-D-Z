4a70591841ae13b517300798fb4ba97e9fba6248
MohonaJS <mohona.chowdhury@iths.se>
2021-12-09 05:33:34 +0100

    object array method(funcntion in object)

--- array/main.js
+++ array/main.js
@@ -5,7 +5,7 @@ const bills = [125,555,44];
 
 /* const bill = 125; */
 
-function calcTip(bill) {   
+/* function calcTip(bill) {   
    if(bill >=50 && bill <=300) {
      const tip =  bill * 0.15
      return tip;   
@@ -19,4 +19,51 @@ const tips = [calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])];
 
 const totals = [tips[0] + bills[0], tips[1] + bills[1],tips[2] + bills[2] ]
 
-console.log(totals); 
+console.log(totals);  */
+
+/* const jonas = {
+    name:"aveen",
+    birthyear:2020,
+    height: true,
+    skills: ['bangla', 'english','swedish'],
+    find: function(){
+        this.age = 2021-this.birthyear;
+        return this.age;
+    },
+    summary: function(){
+   const res =  `her name is ${this.name} and her skills are ${this.skills} and age is ${this.find()}`
+   return res;
+    }
+}
+console.log(jonas.summary()); */
+
+const personOne = {
+    fullName:'mark miller',
+    mass:78,
+    height:1.69,
+    calcBMI: function(){
+       const bmi =  this.mass/this.height;
+        return bmi;
+    }
+}
+const firstbmi= personOne.calcBMI();
+
+const personTwo = {
+    fullName:'john',
+    mass:92,
+    height:1.95,
+    calcBMI: function(){
+       const bmi = this.mass/this.height;
+       return bmi;
+    }
+}
+const secondbmi= personTwo.calcBMI();
+
+if(firstbmi > secondbmi){
+   console.log(`${personOne.fullName} has higher bmi than ${personTwo.fullName}`)
+}else {
+    console.log(`${personOne.fullName} has not higher bmi than ${personTwo.fullName}`)
+}
+
+
+
